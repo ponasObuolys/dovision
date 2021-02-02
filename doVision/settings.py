a600fa2ccb4ceb5a9ae01cdd7daade9bc16b6f9b
JonasSatkauskas <joncius224@gmail.com>
2021-02-02 16:24:50 +0200

    Bandymas sutvarkyti bugus. Išėmiau dbsqlite3 failą iš gitignore.

--- doVision/settings.py
+++ doVision/settings.py
@@ -45,11 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'doVision',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
