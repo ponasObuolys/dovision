df4f9e23686e319b0e715256c7ddfe2463ecee5b
Satkus <satkusmedia@gmail.com>
2021-02-22 18:25:10 +0200

    Reset password "forma"

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, Reset_Password_View, Reset_Password_Done_View
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('reset/', Reset_Password_View, name='reset'),
+    path('reset_done/', Reset_Password_Done_View, name='reset_done')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,20 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm, SetPasswordForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+def Reset_Password_View(request):
+    form = PasswordResetForm
+    context = {'form': form}
+    return render(request, 'registration/reset_password.html', context)
+
+
+def Reset_Password_Done_View(request):
+    return render(request, 'registration/reset_password_done.html')
